e4b4005930c8d31b52caa3beaa690d31cfa364ab
secucos@gmail.com <91136814+Serkiit@users.noreply.github.com>
2023-12-01 01:08:03 +0200

    program

--- Serkiit/Program.cs
+++ Serkiit/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿string[] array1 = new string[4] {"hello", "2", "world", ":-)", }; 
+string[] array2 = new string[array1.Length] ;
+void SecondArreyWithIF(string[] array1, string[]arrey2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length <= 3)
+        {
+            arrey2[count] = array1[i];
+            count++;
+        }
+    }
+}
+
